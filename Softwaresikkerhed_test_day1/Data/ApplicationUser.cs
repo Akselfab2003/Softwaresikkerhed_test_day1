5f912bde25736eb6d0140b62112346f8c44f9c5a
Aksel Fabricius <akselfabricius@gmail.com>
2025-02-24 09:52:02 +0100

    Add project files.

---
+++ Softwaresikkerhed_test_day1/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Softwaresikkerhed_test_day1.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
