8a9b9e217574fbdfdb660f1f6c448787676331d9
Aksel Fabricius <akselfabricius@gmail.com>
2025-02-25 09:54:48 +0100

    Add unit testing project and initial tests for Home component
    Updated the solution to include a new project `Softwaresikkerhed_test_day1.tests` with the necessary configuration. Created the project file targeting .NET 9.0 and added essential testing libraries like `bunit`, `coverlet.collector`, and `xunit`.
    
    Introduced a new test class `UnitTest1` with multiple test methods to verify the `Home` component's behavior under various authorization scenarios using the `bunit` library.

---
+++ Softwaresikkerhed_test_day1.tests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Softwaresikkerhed_test_day1.Components.Pages;
+using Softwaresikkerhed_test_day1.Data;
+using System.Diagnostics.Metrics;
+using System.Security.Claims;
+
+namespace Softwaresikkerhed_test_day1.tests
+{
+    public class UnitTest1 : TestContext
+    {
+
+        [Fact]
+        public void Home_IsNotAuthorized()
+        {
+
+            // Arrange
+
+            this.AddTestAuthorization();
+
+
+            // Act
+
+            var cut = RenderComponent<Home>();
+
+            // Assert
+
+            cut.MarkupMatches(@"<h1>Hello, world!</h1><p>Welcome NotAuthorized user to your new app.</p>");
+
+        }
+
+        [Fact]
+        public void Home_IsAuthorized()
+        {
+            // Arrange
+            var authcontext =  this.AddTestAuthorization();
+            
+            authcontext.SetAuthorized("Test",AuthorizationState.Authorized);
+
+            // Act
+
+            var cut = RenderComponent<Home>();
+
+            // Assert
+
+            cut.MarkupMatches(@"<h1>Hello, world!</h1><p>Welcome user to your new app.</p>");
+        }
+
+
+
+        [Fact]
+        public void Home_IsAuthorizedAndAdmin()
+        {
+            // Arrange
+            var authcontext = this.AddTestAuthorization();
+
+            authcontext.SetAuthorized("Test", AuthorizationState.Authorized);
+            authcontext.SetRoles("Admin");
+
+            // Act
+
+            var cut = RenderComponent<Home>();
+            
+            // Assert
+
+            cut.MarkupMatches(@"<h1>Hello, world!</h1><p>Welcome user to your new app.</p><p>You are Admin.</p>");
+        }
+    }
+}
